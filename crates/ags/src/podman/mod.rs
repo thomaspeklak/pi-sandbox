515160311d5a9f191936507fb518e0bf60f227ec
Thomas Peklak <thomas.peklak@trigo.at>
2026-03-15 22:28:05 +0000

    style: run cargo fmt

--- crates/ags/src/podman/mod.rs
+++ crates/ags/src/podman/mod.rs
@@ -2,4 +2,6 @@ mod args;
 mod exec;
 
 pub use args::build_run_args;
-pub use exec::{PodmanError, ensure_image, execute, image_exists, image_has_binary, write_env_file};
+pub use exec::{
+    PodmanError, ensure_image, execute, image_exists, image_has_binary, write_env_file,
+};
